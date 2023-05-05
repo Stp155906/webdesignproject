99e021ef6a22d62ca5d2275fb5abc635bc3a7543
Shantalia <71754706+Stp155906@users.noreply.github.com>
2023-05-05 19:50:57 -0400

    Updated index.js file

--- index.js
+++ index.js
@@ -4,7 +4,61 @@ import { createClient } from '@supabase/supabase-js';
 // Create a new Supabase client instance with your API URL and API key
 const supabase = createClient('https://<your-project-id>.supabase.co', '<your-anon-key>');
 
-// Define a function to get data from Supabase
+// Define a function to generate a Bootstrap card based on a row of data
+function generateCard(data) {
+  // Create the card element
+  const card = document.createElement('div');
+  card.classList.add('col-md-4', 'mb-4');
+
+  // Create the card contents
+  const image = document.createElement('img');
+  image.classList.add('card-img-top');
+  image.src = data.image_url;
+  image.alt = data.name;
+
+  const body = document.createElement('div');
+  body.classList.add('card-body');
+
+  const title = document.createElement('h5');
+  title.classList.add('card-title');
+  title.textContent = data.name;
+
+  const description = document.createElement('p');
+  description.classList.add('card-text');
+  description.textContent = data.description;
+
+  const link = document.createElement('a');
+  link.classList.add('btn', 'btn-primary');
+  link.href = data.link_url;
+  link.textContent = 'Go somewhere';
+
+  // Add the card contents to the card element
+  body.appendChild(title);
+  body.appendChild(description);
+  body.appendChild(link);
+
+  card.appendChild(image);
+  card.appendChild(body);
+
+  // Return the card element
+  return card;
+}
+
+// Define a function to render the generated cards to the page
+function renderCards(cards) {
+  // Get the container element for the cards
+  const container = document.getElementById('card-container');
+
+  // Remove any existing cards from the container
+  container.innerHTML = '';
+
+  // Add the generated cards to the container
+  for (const card of cards) {
+    container.appendChild(card);
+  }
+}
+
+// Define a function to get data from Supabase and generate cards
 async function getData() {
   try {
     // Query your Supabase database using the "supabase" client
@@ -14,8 +68,11 @@ async function getData() {
       throw error;
     }
 
-    // Log the retrieved data to the console
-    console.log(data);
+    // Generate a card for each row of data
+    const cards = data.map(generateCard);
+
+    // Render the generated cards to the page
+    renderCards(cards);
   } catch (error) {
     console.error(error);
   }
