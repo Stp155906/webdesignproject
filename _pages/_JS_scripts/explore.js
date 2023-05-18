a30a61ae26c86e3f7e919c5dc6a7b40ed58259c3
Shantalia <71754706+Stp155906@users.noreply.github.com>
2023-05-18 15:45:59 -0400

    update

--- _pages/_JS_scripts/explore.js
+++ _pages/_JS_scripts/explore.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-  $('.story-container').on('mousewheel', function(e) {
-    e.preventDefault();
-    var scrollDistance = e.originalEvent.wheelDelta || -e.originalEvent.deltaY;
-    this.scrollLeft -= scrollDistance;
+    $('.story-container').on('mousewheel', function(e) {
+      e.preventDefault();
+      var scrollDistance = e.originalEvent.wheelDelta || -e.originalEvent.deltaY;
+      this.scrollLeft -= scrollDistance;
+    });
   });
-});
+
+  $(function() {
+    $(".card-component-placeholder").load("card-component.html", function() {
+      // Once the card-component.html is loaded, you can perform any additional logic or modifications here.
+      // For example, you can add event listeners or manipulate the loaded content.
+
+      // Example: Add a click event listener to the like-icon
+      $(".like-icon").on("click", function() {
+        // Handle like functionality here
+        console.log("Liked!");
+      });
+    });
+  });
